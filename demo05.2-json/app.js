410b479a8bb2467465505ad8d86cab8a9a5f0813
Bstorm <formation.net@outlook.be>
2020-09-08 15:59:28 +0200

    Demo 5.2

---
+++ demo05.2-json/app.js
@@ -0,0 +1,26 @@
+const {readFile} = require('fs');
+
+const getDataFromJson = (filename) => {
+    return new Promise((resolve, reject) => {
+        readFile(filename, (error, data) => {
+            if(error) {
+                reject(error);
+            }
+            try {
+                const json = JSON.parse(data);
+                resolve(json);
+            } catch (error) {
+                reject(error)
+            }
+        })
+    })
+}
+
+getDataFromJson('./datas/people.json').then(data => {
+    for (const person of data.people) {
+        console.log(`${person.firstname} ${person.lastname}`);
+    }
+}).catch(error => {
+    console.log(`Une erreur c'est produite !`);
+    console.log(error);
+})
